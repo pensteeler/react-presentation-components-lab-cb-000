22c5df43933ee2b4b5033a554c09296861dec5eb
John Vascsinec <pensteeler@gmail.com>
2018-10-24 16:34:41 +0000

    automatically backed up by learn

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // Code SimplerComponent Here
 =======
 import React from 'react';
@@ -6,4 +5,3 @@ import React from 'react';
 const SimplerComponent = props => <div onClick={props.handleClick}>I am just happy.</div>
 
 export default SimplerComponent;
->>>>>>> 90366b341b2158b3bc318890436fc4095f76601a
