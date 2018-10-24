d3b8f52f5644e66246c8886d5d96b143132c63af
John Vascsinec <pensteeler@gmail.com>
2018-10-24 16:31:13 +0000

    automatically backed up by learn

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,26 @@
+<<<<<<< HEAD
 // Code SimpleComponent Here
+=======
+import React, { Component } from 'react';
+
+class DumbComponent extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      mood: 'happy'
+    };
+  }
+
+  handleClick = () => {
+    const newMood = this.state.mood === 'happy' ? 'sad' : 'happy';
+    this.setState({ mood: newMood });
+  }
+
+  render() {
+    return <div onClick={this.handleClick}>{this.state.mood}</div>;
+  }
+}
+
+export default DumbComponent;
+>>>>>>> 90366b341b2158b3bc318890436fc4095f76601a
--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,9 @@
+<<<<<<< HEAD
 // Code SimplerComponent Here
+=======
+import React from 'react';
+
+const SimplerComponent = props => <div onClick={props.handleClick}>I am just happy.</div>
+
+export default SimplerComponent;
+>>>>>>> 90366b341b2158b3bc318890436fc4095f76601a
